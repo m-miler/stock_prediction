a94f04ec834783776a7356b6a5f43d23acff851d
m-miler <mmiler30@gmail.com>
2024-04-10 23:05:09 +0200

    feat: add Model predict method

--- stock_prediction/model/model.py
+++ stock_prediction/model/model.py
@@ -1,23 +1,48 @@
 import numpy as np
 import pickle
 import os
+import requests
 
-from stock_prediction.model.create import ModelCreate
-from stock_prediction.model.info import ModelParameters
+from keras.models import load_model
+from pandas.tseries.offsets import BDay
+from datetime import date, timedelta
 
 
 class Model:
+    base_url = 'http://localhost:8080'
 
     def __init__(self, ticker: str):
         self.ticker = ticker
 
     def predict(self) -> dict:
-        # data = Get latest 3 price stock from stock api database
-        # Get model and scaler
-        # self.scaler.transform(date)
-        # prediction = self.model.prediction
-        # return  {'prediction': self.scaler.inverse_transform(prediction)}
-        pass
+
+        try:
+            model = self.model
+            scaler = self.scaler
+            dates = [(date.today() - BDay(i)).strftime("%Y-%m-%d") for i in range(1, 4)]
+            data = self._get_predict_data(dates)
+
+        except (NotImplementedError, IndexError) as err:
+            return {'error': err.args}
+
+        data = scaler.transform(data.reshape(-1, 1)).reshape(1, -1)
+        data = np.reshape(data, (data.shape[0], 1, data.shape[1]))
+        prediction = model.predict(data)
+        prediction = scaler.inverse_transform(prediction)
+        return {'prediction': float(prediction[0][0])}
+
+    def _get_predict_data(self, dates: list) -> np.array:
+        input_data = []
+        for d in dates:
+            url = f'{self.base_url}/prices/?date={d}&company_abbreviation={self.ticker}'
+            response = requests.get(url).json()
+
+            try:
+                input_data.append(response[0]['close_price'])
+            except IndexError:
+                raise IndexError(f"Missing price data for date: {d}")
+
+        return np.array(input_data)
 
     def get_model_info(self) -> dict:
         # TODO -> if model exists check the previous information in database
@@ -26,9 +51,17 @@ class Model:
         pass
 
     def __getattr__(self, item):
-        if item == "scaler" or item == "model":
-            if os.path.isfile(os.path.join(os.pardir, f"models/{self.ticker}_{item}.pkl")):
-                item = pickle.loads(os.path.join(os.pardir, f"models/{self.ticker}_{item}.pkl"))
+        if item == "scaler":
+            if os.path.isfile(os.path.join(os.path.join(os.getcwd(), fr"models\{self.ticker}_scaler.pkl"))):
+                item = pickle.load(
+                    open(os.path.join(os.path.join(os.getcwd(), fr"models\{self.ticker}_scaler.pkl")), 'rb')
+                )
                 return item
-            return f"{item.title()} has not been implemented yet."
+            raise NotImplementedError(f"{item.title()} for {self.ticker} has not been created yet.")
+        elif item == "model":
+            if os.path.isfile(os.path.join(os.getcwd(), fr"models\{self.ticker}_model.h5")):
+                item = load_model(os.path.join(os.getcwd(), fr"models\{self.ticker}_model.h5"))
+                return item
+            raise NotImplementedError(f"{item.title()} for {self.ticker} has not been created yet.")
+
         return self.item
